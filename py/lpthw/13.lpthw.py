928e4cdadff163e990396b59ee9abc77bb463629
zorbax <otto94@gmail.com>
2016-01-28 17:15:59 -0600

    stuf

---
+++ py/lpthw/13.lpthw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+from sys import argv
+
+script, uno, dos, tres = argv
+
+print "The script is called:", script
+print "Your first variable is:", uno
+print "Your second variable is:", dos
+print "Your third variable is:", tres
