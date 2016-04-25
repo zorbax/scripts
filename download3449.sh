1a5c8d7dd4d1dc0c6d11b0252abfedf6262f4b3d
zorbax <otto94@gmail.com>
2016-04-25 10:43:39 -0500

    scripts

---
+++ download3449.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $(<taxon_id)
+do 
+    name=$(basename "$i")
+    epost -db taxonomy -id $i | elink -target nuccore | efetch -format fasta > id.fa.tmp
+    perl -pe 'if(/\>/){s/\n/\t/}; s/\n//; s/\>/\n\>/' id.fa.tmp | perl -pe 's/\t/\n/' | tail -n+2 > tmp
+    mv tmp id.fa && rm id.fa.tmp
+    cat id.fa | perl -pe 'if(/\>/){s/\n/\#/g}' | grep NC_ | grep -vi plasmid | sed 's/#/\n/g'> $name\_nc.fa
+    rm id.fa
+    echo "done\n"
+done
+
