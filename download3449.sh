8386c5f84547098d2a41160bdaa2f47902d8160b
zorbax <otto94@gmail.com>
2016-05-05 18:31:23 -0500

    stuff

--- download3449.sh
+++ download3449.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+: <<'END'
 
 for i in $(<taxon_id)
 do 
@@ -10,4 +11,15 @@ do
     rm id.fa
     echo "done\n"
 done
+END
 
+for i in $(<taxon_id)
+do
+    name=$(basename "$i")
+    epost -db taxonomy -id $i | elink -target nuccore | efetch -format fasta > id.fa.tmp
+    perl -pe 'if(/\>/){s/\n/\t/}; s/\n//; s/\>/\n\>/' id.fa.tmp | perl -pe 's/\t/\n/' | tail -n+2 > tmp
+    mv tmp id.fa && rm id.fa.tmp
+	cat id.fa | perl -pe 'if(/\>/){s/\n/\@/g}' | sed 's/|ref|/#ref$>/g' | grep \#ref\$\> | grep -v "ribosomal\|plasmid" | cut -d\$ -f2 | sed 's/@/\n/g'> $name\_nc.fa
+    rm id.fa
+    echo "Done $i"
+done
