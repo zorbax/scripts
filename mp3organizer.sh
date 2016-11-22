aa6eb51e13b7544ea1b7ad63d86ce490d5d0393b
zorbax <otto94@gmail.com>
2016-11-22 12:27:38 -0600

    stuff

--- mp3organizer.sh
+++ mp3organizer.sh
@@ -10,7 +10,7 @@ music_collection="$HOME/Music"
 
 # GENERAL usage
 # 1) Edit music_collection setting above if your music isn't in the "normal" place
-# 2) run in a terminal as: ./mp3organizer /path_to/where_the_new_music_files/are 
+# 2) run in a terminal as: ./mp3organizer /path_to/where_the_new_music_files/are
 # Note that the path can't have any spaces.
 
 # NOTES AND ADVANCED USAGE
@@ -27,7 +27,7 @@ music_collection="$HOME/Music"
 
 # Set to "copy" or "move" to make the script either copy files to your music collection and leave
 # the originals where they were or move them to your music collection.
-mode="move"
+mode="copy"
 
 # If you want the unknown files to be saved somewhere else, or to change where this script
 # keeps temporary files, change these:
@@ -45,7 +45,7 @@ if [ -z "$1" ];then
 else
   echo "Working from supplied directory"
   echo "$1"
-    new_music="$1"
+  new_music="$1"
 fi
 
 # Make sure if a folder has been passed that it actuall exists
@@ -88,42 +88,42 @@ cd $working_dir
 # Work on each file one at a time
 for F in ./*
 do
-    if [ -f "$F" ];then
-    # use mminfo to get the track info
-    # LOTS of stuff going on here. Have to remove leading space, special chars and pad track to 0x etc.
-    # Someone smart could do this a lot more elegantly than this!
-        artist=`mminfo "$F"|grep artist|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
-        title=`mminfo "$F"|grep title|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
-        album=`mminfo "$F"|grep album|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
-        trackno=`mminfo "$F"|grep trackno|awk -F: '{print $2}'|sed 's/^ *//g'| awk '{printf "%02d\n", $1;}'`
+  if [ -f "$F" ];then
+  # use mminfo to get the track info
+  # LOTS of stuff going on here. Have to remove leading space, special chars and pad track to 0x etc.
+  # Someone smart could do this a lot more elegantly than this!
+    artist=`mminfo "$F"|grep artist|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
+    title=`mminfo "$F"|grep title|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
+    album=`mminfo "$F"|grep album|awk -F: '{print $2}'|sed 's/^ *//g'|sed 's/[^a-zA-Z0-9\ \-\_]//g'`
+    trackno=`mminfo "$F"|grep trackno|awk -F: '{print $2}'|sed 's/^ *//g'| awk '{printf "%02d\n", $1;}'`
 
-      echo "============================"
-        echo "artist:" $artist
-        echo "title:" $title
-        echo "album:" $album
-        echo "trackno:" $trackno
+    echo "============================"
+    echo "artist:" $artist
+    echo "title:" $title
+    echo "album:" $album
+    echo "trackno:" $trackno
 
-      # If we have an artist, album and title then we know where to nicely put this file
-            if [  -n "$artist"  ] && [ -n "$album" ] && [  -n "$title"  ];then
-          # This is what we are going to call the file
-                filename="$trackno - $title.mp3";
-                if [ -n  "$backup_dir" ];then
-                      cp "$F" "$backup_dir/"
-                  fi
-                # If the right directory structure exists - awesome. Move the file.
-                    if [ -d "$music_collection/$artist/$album" ];then
-                        mv "$F" "$music_collection/$artist/$album/$filename"
-                    else
-                      # If not, build the whole file structure. Won't do any harm to try and create an artist directory that
-                      # is already there - so not even checking for that.
-                        mkdir "$music_collection/$artist"
-                        mkdir "$music_collection/$artist/$album"
-                        mv "$F" "$music_collection/$artist/$album/$filename"
-                    fi
-            else
-        # Don't know who it's by, what album, or what track, so moving it to UNKNOWN
-            mv "$F" "$unknown_dir/"
-                echo -e "UNKNOWN: $F \n"
+    # If we have an artist, album and title then we know where to nicely put this file
+      if [  -n "$artist"  ] && [ -n "$album" ] && [  -n "$title"  ];then
+            # This is what we are going to call the file
+            filename="$trackno - $title.mp3";
+            if [ -n  "$backup_dir" ];then
+              cp "$F" "$backup_dir/"
             fi
+            # If the right directory structure exists - awesome. Move the file.
+            if [ -d "$music_collection/$artist - $album" ];then
+              mv "$F" "$music_collection/$artist - $album/$filename"
+            else
+            # If not, build the whole file structure. Won't do any harm to try and create an artist directory that
+            # is already there - so not even checking for that.
+            #mkdir "$music_collection/$artist"
+              mkdir "$music_collection/$artist - $album"
+              mv "$F" "$music_collection/$artist - $album/$filename"
+            fi
+      else
+        # Don't know who it's by, what album, or what track, so moving it to UNKNOWN
+          mv "$F" "$unknown_dir/"
+          echo -e "UNKNOWN: $F \n"
+      fi
     fi
 done
