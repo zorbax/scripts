11213024357701422a2b4e4e8103e5140254b5c4
zorbax <otto94@gmail.com>
2017-08-10 20:31:12 -0500

    one_line 01

--- python/fq2fa01.py
+++ python/fq2fa01.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python
-
 """
 FASTQ to FASTA 01
 """
-
-
 from Bio import SeqIO
 import sys
 import os
 
 def main():
     if len(sys.argv) != 3:
-        print >> sys.stderr, 'Usage: python %s <input.fastq> <output.fna>'\
+        print >> sys.stderr, 'Usage: python %s <input.fastq> <output.fna>' \
                  %(os.path.basename(sys.argv[0]))
         sys.exit(1)
 
     infile = sys.argv[1]
     outfile = sys.argv[2]
 
-    n = SeqIO.convert(infile, "fastq", outfile, "fasta")
+    fasta_out = FastaIO.FastaWriter(SeqIO.convert(infile, "fastq", outfile, "fasta"), wrap=None)
+    fasta_out.write_file()
 
 if __name__ == '__main__':
     main()
--- python/fq2fa02.py
+++ python/fq2fa02.py
@@ -2,13 +2,12 @@
 """
 FASTQ to FASTA 02
 """
-
 import sys, os
 import screed
 
 def main():
     if len(sys.argv) != 3:
-        print >> sys.stderr, 'Usage: python %s <input.fastq> <output.fna>'\
+        print >> sys.stderr, 'Usage: python %s <input.fastq> <output.fna>' \
                  %(os.path.basename(sys.argv[0]))
         sys.exit(1)
 
@@ -21,7 +20,6 @@ def main():
         seq = record['sequence']
         print >> wrfile, '>%s\n%s' %(name, seq)
 
-    #print (n+1), 'fasta seqs written'
     wrfile.close()
 
 if __name__ == '__main__':
