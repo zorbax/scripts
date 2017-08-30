179ed207a064d524bbf2f52d63a005d8a250f0c4
zorbax <otto94@gmail.com>
2017-08-29 22:42:24 -0500

    ##

--- python/05_download_sra.py
+++ python/05_download_sra.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 from __future__ import print_function
+import sys
 import subprocess
 
-srrs = [ "SRR5921431", "SRR5888364", "SRR5888413",
-        "SRR5932688", "SRR5921432", "SRR5921441", "SRR5921437"]
-
-with open('acc.txt', 'r') as srrid:
-    srrs = srrid.read().split("\n")
+with open(sys.argv[1], 'r') as srr_id:
+    srrs = srr_id.read().split("\n")
 
 for i in srrs:
     print("Downloading %s" % i)
-    subprocess.call('fastq-dump -I --gzip --split-files %s' % i, shell=True)
-
-
-for acc in accs:
-    subprocess.call('srst2 --input_se %s_1.fastq %s_2.fastq --log --gene_db gene.db --output %s' % (acc, acc, acc), shell=True)
-    subprocess.call('rm %s_1.fastq %s_2.fastq' % (acc, acc), shell=True)
+#    subprocess.call('prefetch -a "/opt/aspera/bin/ascp|/opt/aspera/etc/asperaweb_id_dsa.openssh" %s' % i, shell=True)
+#    print("Converting SRA > FASTQ")
+     subprocess.call('fastq-dump -I --gzip --split-files %s' % i, shell=True)
+#    subprocess.call('rm %s.sra' % i, shell=True)
+   #subprocess.call('srst2 --input_se %s_1.fastq %s_2.fastq --log --gene_db gene.db --output %s' % (i, i, i), shell=True)
