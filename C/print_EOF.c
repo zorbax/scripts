65358ac6fe1e341af4d79aaa3903a6b8ae826976
zorbax <otto94@gmail.com>
2018-03-13 22:13:27 -0600

    remove

--- C/print_EOF.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-/*print thr value of EOF */
-
-main() {
-    printf("EOF: %d\n", EOF);
-}
--- C/verify_EOF.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-/* Verify that the expression `getchar() != EOF` is 0 or 1. */
-
-main() {
-    printf("%d\n", getchar() != EOF);
-}
