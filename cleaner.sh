ad380972db3d6054959cf0f0a988492c04d1c428
Otoniel Maya <otto94@gmail.com>
2019-03-25 13:44:09 -0600

    cleaning

---
+++ cleaner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo=`which sudo`
+
+if [ "`whoami`" == "root" ]; then
+  root=true
+fi
+
+if [ ! "`which sudo`" ] && [ !$root ]; then
+  echo "Not Superuser, and sudo not found!"
+  exit 1
+fi
+
+memfree=`cat /proc/meminfo | grep MemFree | awk '{ print $2 }'`
+freemem=`echo "$memfree/1024.0" | bc`
+
+if [ "$?" != "0" ]; then
+  echo "Something went wrong. Run again."
+  exit 1
+fi
+
+if [ $root ]; then
+  free -h && sync
+  echo 3 > /proc/sys/vm/drop_caches > /dev/null && free -h
+else
+  free -h && $sudo sync
+  echo 3 | $sudo tee /proc/sys/vm/drop_caches > /dev/null && free -h
+fi
+
+after=`cat /proc/meminfo | grep MemFree | awk '{ print $2 }'`
+after=`echo "$after/1024.0" | bc`
+
+echo -e "\nThis freed `echo "$after - $freemem" | bc` Mb."
+
+exit 0
---
+++ rsync_retry.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+trap "echo ----; exit;" SIGINT SIGTERM
+
+start=$1
+end=$2
+retries=24
+i=0
+
+false
+
+while [ $? -ne 0 -a $i -lt $retries ]
+do
+  i=$(( $i + 1 ))
+  rsync -aPvz $start $end
+done
+
+if [ $i -eq $retries ]; then
+  echo "Hit maximum number of retries, giving up."
+fi
