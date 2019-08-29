db470fb5934c9ef8195a48d3d06d0202b03fe1e9
Otoniel Maya <otto94@gmail.com>
2019-08-28 23:18:34 -0500

    rm sra

--- bash/aspera.sh
+++ bash/aspera.sh
@@ -10,6 +10,10 @@ do
   xrr=${line:0:3}
   fasp_url=${base_url}/${xrr}/${srr_prefix}/${i}/${i}.sra
 
+  bw_mbps_limit=80
+  #ascp -i ${key} -k1 -QTr -l${bw_mbps_limit}m \
+  #      ${base-url}/${i:0:3}/${i:0:6}/${i%.sra}/${i} .
+
   if [[ ! -e ${i%.sra}.aspx && ! -e ${i%.sra}.fastq.gz ]];
     echo "${i}.sra already exists, skip..."
     echo "SRA > FASTQ"
@@ -17,7 +21,7 @@ do
                  --defline-qual '+' --gzip ${i}.sra
   else
     echo "Downloading ${i}"
-    ascp -i ${key} -k3 -T -l20m -m1k --policy=fair ${fasp_url} .
+    ascp -i ${key} -k3 -T -l{bw_mbps_limit}m -m1k --policy=fair ${fasp_url} .
     echo "SRA > FASTQ"
     fastq-dump --split-files --readids --defline-seq '@$ac.$si/$ri' \
                --defline-qual '+' --gzip ${i}.sra
