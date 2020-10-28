ad70180ae0a378293b0d79c9edad69e0d9392d6d
zorbax <otto94@gmail.com>
2020-10-28 10:22:42 +0100

    python/12_debug.py

--- python/12_debug.py
+++ python/12_debug.py
@@ -1,38 +1,26 @@
-import random
+import sys
+import argparse
+import logging
+from logging import critical, error, info, warning, debug
 
 
-def generate_random(upper):
-    """
-    :param upper: >= 0
-    :return: int
-    """
-    r = random.randint(1, upper)
-    return r
+def parse_arguments():
+    """Read arguments from a command line."""
+    parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
+    parser.add_argument('-v', metavar='verbosity', type=int, default=2,
+                        help='Verbosity of logging: 0 -critical, 1- error, 2 -warning, 3 -info, 4 -debug')
+
+    args = parser.parse_args()
+    verbose = {0: logging.CRITICAL, 1: logging.ERROR, 2: logging.WARNING, 3: logging.INFO, 4: logging.DEBUG}
+    logging.basicConfig(format='%(message)s', level=verbose[args.v], stream=sys.stdout)
+
+    return args
 
 
 def main():
-    run = True
-    num1 = generate_random(10)
-    num2 = generate_random(10)
-    result = num1 * num2
-    while run:
-        ans = input("What is " + str(num1) + " x " + str(num2) + "? ")
-
-        if ans.isdigit():
-            if int(ans) == result:
-                print("Correct!")
-                run = False
-            else:
-                print("Incorrect! Try again.")
-        else:
-            print("Answer must be a positive number, try again.")
+    pass
 
 
-times = 10
-
-for x in range(times):
+if __name__ == '__main__':
+    args = parse_arguments()
     main()
-
-print(__name__)
-
-
