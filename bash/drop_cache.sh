63c493e849c9f724abdc7f75444eb5d8022a9cbc
zorbax <otto94@gmail.com>
2021-06-09 14:27:19 +0200

    dropcache

--- bash/drop_cache.sh
+++ bash/drop_cache.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-sudo=`which sudo`
+sudo=$(which sudo)
 
-if [ "`whoami`" == "root" ]; then
+if [ "$(whoami)" == "root" ]; then
   root=true
 fi
 
-if [ ! "`which sudo`" ] && [ !$root ]; then
-  echo "Not Superuser, and sudo not found!"
-  exit 1
+if [ ! "$(which sudo)" ] && [ !$root ]; then
+    echo "Not Superuser, and sudo not found!"
+    exit 1
 fi
 
-memfree=`cat /proc/meminfo | grep MemFree | awk '{ print $2 }'`
-freemem=`echo "$memfree/1024.0" | bc`
+memfree=$(grep MemFree /proc/meminfo | awk '{ print $2 }')
+freemem=$(echo "$memfree/1024.0" | bc)
 
 if [ "$?" != "0" ]; then
-  echo "Something went wrong. Run again."
-  exit 1
+    echo "Something went wrong. Run again."
+    exit 1
 fi
 
 if [ $root ]; then
-  free -h && sync
-  echo 3 > /proc/sys/vm/drop_caches > /dev/null && free -h
+    free -h && sync
+    echo 3 > /proc/sys/vm/drop_caches > /dev/null && free -h
 else
-  free -h && $sudo sync
-  echo 3 | $sudo tee /proc/sys/vm/drop_caches > /dev/null && free -h
+    free -h && $sudo sync
+    echo 3 | $sudo tee /proc/sys/vm/drop_caches > /dev/null && free -h
 fi
 
-after=`cat /proc/meminfo | grep MemFree | awk '{ print $2 }'`
-after=`echo "$after/1024.0" | bc`
+after=$(grep MemFree /proc/meminfo | awk '{ print $2 }')
+after=$(echo "$after/1024.0" | bc)
 
-echo -e "\nThis freed `echo "$after - $freemem" | bc` Mb."
-
+echo -e "\nThis freed $(echo "$after - $freemem" | bc) Mb."
 exit 0
