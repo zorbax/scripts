6ac43ce821e4e3c57589d9fb89eb0837303cb20a
zorbax <otto94@gmail.com>
2022-01-25 20:12:32 +0100

    one

--- bash/one_liner.sh
+++ bash/one_liner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 tmpfile=$(mktemp $PWD/XXXX)
-awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} \
-    {printf("%s",$0);} END {printf("\n");}' < $1 > ${tmpfile} && mv ${tmpfile} $1
+perl -pe 'if(/\>/){s/$/\t/}; s/\n//g; s/\>/\n\>/g' $1 | \
+    perl -pe 's/\t/\n/g' ${tmpfile} && mv ${tmpfile} $1
