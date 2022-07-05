e95819cb02bb6a8d27cf1721f1f928935d71f197
zorbax <otto94@gmail.com>
2022-07-05 22:31:24 +0200

    one

--- bash/one_liner.sh
+++ bash/one_liner.sh
@@ -2,4 +2,4 @@
 
 tmpfile=$(mktemp $PWD/XXXX)
 perl -pe 'if(/\>/){s/$/\t/}; s/\n//g; s/\>/\n\>/g' $1 | \
-    perl -pe 's/\t/\n/g' ${tmpfile} && mv ${tmpfile} $1
+    perl -pe 's/\t/\n/g' > ${tmpfile} && mv ${tmpfile} $1
