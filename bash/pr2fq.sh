ab92573552afdb8a35e04c7f96d70d4a80645fe9
Otoniel Maya <otto94@gmail.com>
2023-01-05 19:39:53 +0100

    api - minor fix

--- bash/pr2fq.sh
+++ bash/pr2fq.sh
@@ -2,12 +2,12 @@
 
 file_report="https://www.ebi.ac.uk/ena/portal/api/filereport?accession="
 options="&result=read_run&fields=fastq_ftp&download=true"
-id="$1"
+id="PRJNA597997"
 links=$(curl --silent "${file_report}""${id}""${options}" | tail -n+2 | cut -f2 | sed 's/;/\n/')
-mkdir -p 00_raw/
+mkdir -p ${id}
 for i in ${links}
 do
     name=$(basename "${i}")
     echo "${name%%.*}"
-    cd 00_raw/ && { curl --silent -o "${name}" -O "${i}" ; cd - > /dev/null || return; }
+    cd ${id} && { curl --silent -o "${name}" -O "${i}" ; cd - > /dev/null || return; }
 done
